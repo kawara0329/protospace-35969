73f79f2b6cddac423a2caf7bea8e49b99c1587d4
kawara0329 <a.kawara0329@gmail.com>
2021-06-24 17:25:01 +0900

    move_to_index修正commit

--- app/controllers/Prototypes_controller.rb
+++ app/controllers/Prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -54,7 +54,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-   unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+   unless 
+    current_user.id == @prototype.user.id
     redirect_to action: :index
    end
   end
